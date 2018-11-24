a03247f98d256facf5184b11f6b4d9059e2fa2e6
andys8 <andys8@users.noreply.github.com>
2018-11-24 19:11:46 +0100

    Link i3 directory

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Link
-ln -sf ~/dotfiles/vimrc                ~/.vimrc
-ln -sf ~/dotfiles/vimrc.local          ~/.vimrc.local
-ln -sf ~/dotfiles/vimrc.local.bundles  ~/.vimrc.local.bundles
+ln -sf ~/dotfiles/vimrc                 ~/.vimrc
+ln -sf ~/dotfiles/vimrc.local           ~/.vimrc.local
+ln -sf ~/dotfiles/vimrc.local.bundles   ~/.vimrc.local.bundles
 
+rm -rf ~/.config/i3
+ln -sf ~/dotfiles/i3                    ~/.config
 
 # Vim plugin installation
 vim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
