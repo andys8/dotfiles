315990aaa7921cc175764cced4d41b2fec58536a
andys8 <andys8@users.noreply.github.com>
2018-11-24 17:22:39 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Link
+ln -sf ~/dotfiles/vimrc                ~/.vimrc
+ln -sf ~/dotfiles/vimrc.local          ~/.vimrc.local
+ln -sf ~/dotfiles/vimrc.local.bundles  ~/.vimrc.local.bundles
