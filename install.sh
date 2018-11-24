f2a4597abbb993e49bb1862ee1fbdabb9e514914
andys8 <andys8@users.noreply.github.com>
2018-11-24 17:31:30 +0100

    Setup steps in install script

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@
 ln -sf ~/dotfiles/vimrc                ~/.vimrc
 ln -sf ~/dotfiles/vimrc.local          ~/.vimrc.local
 ln -sf ~/dotfiles/vimrc.local.bundles  ~/.vimrc.local.bundles
+
+
+# Vim plugin installation
+vim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
+
+# Setup YouCompleteMe
+python3 ~/.vim/plugged/youcompleteme/install.py
