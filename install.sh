ebbe4023034098c252ff92c7f726457ab46ea9be
andys8 <andys8@users.noreply.github.com>
2018-12-02 23:31:17 +0100

    link and update script

--- install.sh
+++ install.sh
@@ -1,28 +1,4 @@
 #!/usr/bin/env bash
 
-# Vim
-ln -sf ~/dotfiles/vimrc                 ~/.vimrc
-ln -sf ~/dotfiles/vimrc.local           ~/.vimrc.local
-ln -sf ~/dotfiles/vimrc.local.bundles   ~/.vimrc.local.bundles
-
-# Ctags
-ln -sf ~/dotfiles/ctags                 ~/.ctags
-
-# i3
-rm -rf ~/.config/i3
-ln -sf ~/dotfiles/i3                    ~/.config
-
-# rofi (dmenu for i3)
-rm -rf ~/.config/rofi
-ln -sf ~/dotfiles/rofi                  ~/.config
-
-# Git
-mkdir -p ~/.config/git
-ln -sf ~/dotfiles/git/ignore            ~/.config/git/ignore
-
-
-# Vim plugin installation
-vim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
-
-# Setup YouCompleteMe
-python3 ~/.vim/plugged/youcompleteme/install.py
+./link.sh
+./update.sh
---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Vim plugin installation
+vim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
+
+# Setup YouCompleteMe
+python3 ~/.vim/plugged/youcompleteme/install.py
+
