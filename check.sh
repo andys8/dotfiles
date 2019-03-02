6cd6d460541a1e9ce0832d5ed099c494965a8fdc
andys8 <andys8@users.noreply.github.com>
2019-03-02 17:47:05 +0100

    check.sh improved

--- check.sh
+++ check.sh
@@ -1,42 +1,55 @@
 #!/usr/bin/env bash
 
-check() {
-  command -v "$1" >/dev/null 2>&1 || { echo "'$1' missing"; exit 1; }
-}
+# Dependencies
+commands=(
+  bash
+  rofi
+  i3
+  i3-sensible-terminal
+  i3-dmenu-desktop
+  i3-nagbar
+  i3lock
+  xrandr
+  gnome-screenshot
+  volumeicon
+  nm-applet
+  python3
+  vim
+  brittany
+  ghc-mod
+  prettier
+  elm-format
+  hlint
+  hdevtools
+  alacritty
+  fish
+  eslint
+  rg
+  gopass
+  toilet
+  diff-so-fancy
+  vimdiff
+  vim
+  shellcheck
+  ~/bin/startup.sh
+)
 
-# Software
-check bash
-check rofi
-check i3
-check i3-sensible-terminal
-check i3-dmenu-desktop
-check i3-nagbar
-check i3lock
-check xrandr
-check gnome-screenshot
-check volumeicon
-check nm-applet
-check python3
-check vim
-check brittany
-check ghc-mod
-check prettier
-check elm-format
-check hlint
-check hdevtools
-check alacritty
-check fish
-check eslint
-check rg
-check gopass
-check toilet
-check diff-so-fancy
-check vimdiff
-check vim
-check shellcheck
+files=(
+  ~/.gitconfig.user
+  ~/bin/startup.sh
+)
 
-# Scripts
-check ~/bin/startup.sh
+check() { command -v "$1" >/dev/null 2>&1 || { echo "'$1' missing"; exit 1; } }
+exists() { if [ ! -f "$1" ]; then echo "'$1' missing"; exit 1; fi }
 
+# Check commands
+for i in "${commands[@]}"
+do check "$i"; done
+
+# Verify files exist
+for i in "${files[@]}"
+do exists "$i"; done
+
+# Otherwise good case
 echo "All dependencies are available"
 exit 0
