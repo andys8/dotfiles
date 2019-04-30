756185cf41a12f3505bf870ad07dc526181971c2
andys8 <andys8@users.noreply.github.com>
2019-04-30 11:00:05 +0200

    Colors in man pages and less

--- config.fish
+++ config.fish
@@ -47,6 +47,15 @@ function sudo
   end
 end
 
+# Colors in man pages / less
+set -x LESS_TERMCAP_mb (set_color -o magenta)
+set -x LESS_TERMCAP_md (set_color -o brgreen)
+set -x LESS_TERMCAP_so (set_color -b blue bryellow)
+set -x LESS_TERMCAP_us (set_color -u brred)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_ue (set_color normal)
+set -x LESS_TERMCAP_se (set_color normal)
+
 # Abbreviations
 if not set -q abbrs_initialized
   set -U abbrs_initialized
