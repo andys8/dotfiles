813a9099e929aecf52e8dd92e6d0669561d59af2
andys8 <andys8@users.noreply.github.com>
2019-07-24 23:34:31 +0200

    Nix: Add exa and diff-so-fancy

--- manifest.nix
+++ manifest.nix
@@ -5,7 +5,8 @@ let
     sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
   }) {};
 in {
-  inherit (pkgs) nix;
+  inherit (pkgs) nix exa;
+  inherit (pkgs.gitAndTools) diff-so-fancy;
   inherit (pkgs.elmPackages) elm elm-format elm-test;
   inherit (fixed) hello cargo scala sbt;
 }
