5685f031184c11f9e4da8decacf16fc6dfcdf391
andys8 <andys8@users.noreply.github.com>
2019-07-24 23:20:51 +0200

    Update: NPM

--- update.sh
+++ update.sh
@@ -8,6 +8,10 @@ nix-env -irf manifest.nix
 # Vim plugin installation
 vim +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean! +qall
 
+# Update version managers
+fish -c "omf update"
+npm i -g npm
+
 # Npm installable dependencies
 npminstallations=(
   @elm-tooling/elm-language-server@1.1.1
@@ -18,7 +22,4 @@ npminstallations=(
 
 for i in "${npminstallations[@]}"
 do (npm install -g "$i"); done
-
-# Update version managers
-fish -c "omf update"
 
