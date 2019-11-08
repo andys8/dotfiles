da129b4319d1998cc29cb49eaab92a5b002a3257
andys8 <andys8@users.noreply.github.com>
2019-11-08 20:31:58 +0100

    NPM: install elm-json by default

--- scripts/update-npm.sh
+++ scripts/update-npm.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 # Npm installable dependencies
 npm set prefix ~/.npm-global
 npminstallations=(
-  markdownlint-cli
-  prettier
-  typescript
+  elm-json # update and change elm.json files
+  markdownlint-cli # markdown linter
+  prettier # js/ts/css formatter
+  typescript # ts compiler
 )
 
 for i in "${npminstallations[@]}"; do (npm install -g "$i")
