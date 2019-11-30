67512842123564aad963298730a9b6c790c1fa27
andys8 <andys8@users.noreply.github.com>
2019-11-30 20:12:08 +0100

    Link: Create vim directory

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Vim
+mkdir -p ~/.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/vimrc.local ~/.vimrc.local
 ln -sf ~/dotfiles/vimrc.local.bundles ~/.vimrc.local.bundles
