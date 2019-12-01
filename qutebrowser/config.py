c7b3cfbd412f458e4b8cbba4c65a60f52f713e22
andys8 <andys8@users.noreply.github.com>
2019-12-01 19:14:19 +0100

    Qutebrowser: SauceCodePro Nerd font

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -792,7 +792,7 @@ c.fonts.keyhint = '12pt monospace'
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
 ## Type: Font
-# c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+c.fonts.monospace = '"SauceCodePro Nerd Font", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
 
 ## Font used for prompts.
 ## Type: Font
