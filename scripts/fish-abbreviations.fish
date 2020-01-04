55f80798edd477904bd2a11bc09355470683ecb9
andys8 <andys8@users.noreply.github.com>
2020-01-04 11:00:42 +0100

    Fish abbreviations: Update and install for pacman

--- scripts/fish-abbreviations.fish
+++ scripts/fish-abbreviations.fish
@@ -7,6 +7,7 @@ abbr -a -U -- ackf 'ack -f | ack'
 abbr -a -U -- agc 'sudo apt-get autoclean'
 abbr -a -U -- agi 'sudo apt-get install'
 abbr -a -U -- agr 'sudo apt-get remove'
+abbr -a -U -- agu 'sudo apt-get update && sudo apt-get upgrade'
 abbr -a -U -- cheat 'curl https://cheat.sh/'
 abbr -a -U -- g git
 abbr -a -U -- ga 'git add'
@@ -30,11 +31,13 @@ abbr -a -U -- n npm
 abbr -a -U -- nr 'npm run'
 abbr -a -U -- ns 'nix-env -qaP | grep'
 abbr -a -U -- open xdg-open
+abbr -a -U -- pai 'pacman -S'
+abbr -a -U -- par 'pacman -Rs'
+abbr -a -U -- pau 'pacman -Syu'
 abbr -a -U -- r ranger
 abbr -a -U -- sdkman sdk
 abbr -a -U -- t tig
 abbr -a -U -- tl 'toilet -t -F border -f future --gay'
-abbr -a -U -- update 'sudo apt-get update && sudo apt-get upgrade'
 abbr -a -U -- v vim
 abbr -a -U -- vi vim
 abbr -a -U -- öö ll
