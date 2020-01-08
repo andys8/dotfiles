c6db337e4112e8d09c7729f259075b0952bec41b
andys8 <andys8@users.noreply.github.com>
2020-01-08 13:38:04 +0100

    hostip: Set HOST_IP actually globally

--- fish/hostip.fish
+++ fish/hostip.fish
@@ -2,6 +2,7 @@
 function hostip
     set ip (ip route get 1 | grep -E -o 'src[[:space:]][0-9]+[.][0-9]+[.][0-9]+[.][0-9]+' | awk '{print $2}')
     # Explicitly export as enviroment variable
-    set -gx HOST_IP $ip
+    set -e HOST_IP
+    set -Ux HOST_IP $ip
     echo $ip
 end
