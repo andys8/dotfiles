32cfbe1a05d1904af3ace9c5356d001379c1458c
andys8 <andys8@users.noreply.github.com>
2020-01-27 11:17:09 +0100

    Fish: copy-screenshot

--- scripts/fish-abbreviations.fish
+++ scripts/fish-abbreviations.fish
@@ -9,6 +9,7 @@ abbr -a -U -- agi 'sudo apt-get install'
 abbr -a -U -- agr 'sudo apt-get remove'
 abbr -a -U -- agu 'sudo apt-get update && sudo apt-get upgrade'
 abbr -a -U -- cheat 'curl https://cheat.sh/'
+abbr -a -U -- copy-screenshot 'cp (ls -t ~/Pictures/screenshot-* | head -n1) .'
 abbr -a -U -- dco docker-compose
 abbr -a -U -- g git
 abbr -a -U -- ga 'git add'
