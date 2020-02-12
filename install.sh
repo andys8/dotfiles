d546811153a87dd8df6471e9ffe24ea8c697846c
andys8 <andys8@users.noreply.github.com>
2020-02-12 22:22:04 +0100

    Install script: Print headlines

--- install.sh
+++ install.sh
@@ -1,24 +1,34 @@
 #!/bin/sh
 set -eu pipefail
 
+section() {
+	echo "$(tput setaf 2)>>> $1$(tput sgr 0)"
+}
+
+section "Install and update dotfiles"
+
+section "Update repository"
 ./scripts/update-repository.sh
 
-# Link files and directories
-./scripts/link.sh
-
-# Checks before
+section "Check pre-conditions"
 ./scripts/check-preconditions.sh
 
-# Updates
+section "Create symbolic links"
+./scripts/link.sh
+
+section "Update Nix"
 ./scripts/update-nix.sh
+section "Update Vim"
 ./scripts/update-vim.sh
+section "Update Fish"
 ./scripts/update-fish.fish
+section "Update Fish abbreviations"
+./scripts/fish-abbreviations.fish
+section "Update Node/NPM"
 ./scripts/update-npm.sh
+section "Update Xmonad"
 ./scripts/update-xmonad.sh
 
-# Abbreviations
-./scripts/fish-abbreviations.fish
-
-# Checks afterwards
+section "Check post-conditions"
 ./scripts/check-preconditions.sh
 ./scripts/check-postconditions.sh
