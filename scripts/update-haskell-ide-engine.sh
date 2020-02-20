6abddfa6427650387e94603bb6c069999be04dfe
andys8 <andys8@users.noreply.github.com>
2020-02-21 00:57:29 +0100

    Haskell-ide-engine: 0.14.0 works better on ubuntu

--- scripts/update-haskell-ide-engine.sh
+++ scripts/update-haskell-ide-engine.sh
@@ -5,7 +5,7 @@ LINUX=$(lsb_release -i -s)
 
 if [ "$LINUX" = "LinuxMint" ]; then
 
-	HASH="d98bdbff3ebdab408a12a9b7890d4cf400180839" # 1.1.0
+	HASH="c6e93d2d641ef48703eabed8ec5cde3d774cb0e5" # 0.14.0.0
 	cachix use all-hies
 	nix-env -iA selection --arg selector 'p: { inherit (p) ghc865; }' -f https://github.com/infinisil/all-hies/tarball/$HASH
 	echo ">> haskell-ide-engine installed with nix"
