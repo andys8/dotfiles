483091f64db35568fa3a587b27246c54f5982089
andys8 <andys8@users.noreply.github.com>
2020-02-23 20:27:54 +0100

    Move install.sh to scripts/install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,3 @@
 #!/bin/sh
 set -eu pipefail
-
-section() {
-	echo "$(tput setaf 2)>>> $1$(tput sgr 0)"
-}
-
-section "Update dotfiles repository"
-~/dotfiles/scripts/update-dotfiles.sh
-
-section "Check pre-conditions"
-~/dotfiles/scripts/check-preconditions.sh
-
-section "Create symlinks"
-~/dotfiles/scripts/link.sh
-
-section "Update Nix"
-~/dotfiles/scripts/update-nix.sh
-section "Update Vim"
-~/dotfiles/scripts/update-vim.sh
-section "Update Fish"
-~/dotfiles/scripts/update-fish.fish
-section "Update Fish abbreviations"
-~/dotfiles/scripts/fish-abbreviations.fish
-section "Update Node/NPM"
-~/dotfiles/scripts/update-npm.sh
-section "Update Xmonad"
-~/dotfiles/scripts/update-xmonad.sh
-section "Update haskell-ide-engine"
-~/dotfiles/scripts/update-haskell-ide-engine.sh
-section "Update ranger plugins"
-~/dotfiles/scripts/update-ranger.sh
-section "Update htop with vim bindings"
-~/dotfiles/scripts/update-htop-vim.sh
-
-section "Check post-conditions"
-~/dotfiles/scripts/check-preconditions.sh
-~/dotfiles/scripts/check-postconditions.sh
+~/dotfiles/scripts/install.sh
