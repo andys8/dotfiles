fec647c8b6f38125b1090c9ead12c78a54f39f7f
andys8 <andys8@users.noreply.github.com>
2020-03-14 15:49:14 +0100

    Script to format json, yaml, nix, markdown, shell

---
+++ scripts/format-dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+
+cd ~/dotfiles
+
+echo "Format shell scripts"
+shfmt -w .
+
+echo "Format markdown, yaml and json"
+prettier --write "**/*.{md,json,yaml,yml}"
+
+echo "Format nix"
+nixpkgs-fmt ./*.nix
