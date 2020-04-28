2c9f6cac30adad9f6f772b681a7f3afbade71dca
andys8 <andys8@users.noreply.github.com>
2020-04-28 21:13:41 +0200

    Abbreviation: htop -> btm (for now, to get used)

--- scripts/fish-abbreviations.fish
+++ scripts/fish-abbreviations.fish
@@ -43,6 +43,7 @@ abbr -a -U -- grm 'git rebase -i origin/master'
 abbr -a -U -- gs 'git status'
 abbr -a -U -- gst 'git stash'
 abbr -a -U -- gti git
+abbr -a -U -- htop btm
 abbr -a -U -- jd 'cd ~/dotfiles'
 abbr -a -U -- lg lazygit
 abbr -a -U -- ll 'lsd -Al --date relative --group-dirs first'
