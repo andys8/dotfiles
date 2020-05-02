5c49d12e8c60e4bdb1b613e9d1d207c440529bca
andys8 <andys8@users.noreply.github.com>
2020-05-02 21:32:27 +0200

    st: update to 0.8.3 branch

--- nix/st.nix
+++ nix/st.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 with stdenv.lib;
 let
   name = "st";
-  version = "0.8.2-andys8";
+  version = "0.8.3-andys8";
 in
 stdenv.mkDerivation rec {
   inherit name;
@@ -11,8 +11,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "andys8";
     repo = "st";
-    rev = "aaa663776c0fbf0b42473685e53d40e1dd29a25f";
-    sha256 = "04qa0kx9a8ny4r896kr9myp4275hd2dfbn62y70j094yh5lh23mn";
+    rev = "d6456cd02629822a408ccde94afdd2d1ef97859d";
+    sha256 = "0ihcdi2y477kgf55a0gr8dfs5wia805m3ni86lbwvf1d34daj1d8";
   };
 
   nativeBuildInputs = [ pkgconfig ];
