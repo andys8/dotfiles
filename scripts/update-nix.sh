c3b3e5a7d3e2218445e4a2eb82aa54eae96d14e4
andys8 <andys8@users.noreply.github.com>
2020-05-03 19:35:34 +0200

    Nix: Sometimes delete garbage with -d
    Cause: There was 60GB of undeleted garbage

--- scripts/update-nix.sh
+++ scripts/update-nix.sh
@@ -7,7 +7,9 @@ nix-channel --update
 # Install packages
 nix-env -irf ~/dotfiles/nix/packages.nix
 
-# Collect garbage (chance ~ 5%)
-[ $((RANDOM % 20)) -eq 0 ] && nix-collect-garbage --delete-older-than 14d
+# Collect garbage (randomly)
+RAND=$((RANDOM % 20))
+[ $((RAND)) -eq 0 ] && nix-collect-garbage --delete-older-than 14d
+[ $((RAND)) -eq 1 ] && nix-collect-garbage -d
 
 echo "Nix packages updated"
