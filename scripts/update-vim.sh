7fb4e6c840c7cb1dd62427d0f72c493d454c24f5
andys8 <andys8@users.noreply.github.com>
2020-06-28 17:31:59 +0200

    Vim: Update Script will update coc extensions

--- scripts/update-vim.sh
+++ scripts/update-vim.sh
@@ -12,6 +12,6 @@ echo "$(tput setaf 8)Update Vim plugins$(tput sgr 0)"
 vim +PlugUpgrade +PlugUpdate +PlugClean! +qall
 
 echo "$(tput setaf 8)Update CoC extensions$(tput sgr 0)"
-vim +CocInstall +qall
+vim +CocInstall +CocUpdateSync +qall
 
 echo "Vim updated"
