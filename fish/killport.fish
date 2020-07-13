55f4317d48045e2c57605ae726ba86fffc87775e
andys8 <andys8@users.noreply.github.com>
2020-07-13 17:13:49 +0200

    killport: kill multiple process ids

--- fish/killport.fish
+++ fish/killport.fish
@@ -1,12 +1,14 @@
 function killport
     if test -n "$argv"
         set port $argv[1]
-        set pid (lsof -ti :$port)
-        if test -n "$pid"
-            kill $pid
-            echo Killed process with pid $pid and port $port
-        else
+        set pids (lsof -ti :$port)
+        if test -z "$pids"
             echo No process with port $port
+        else
+            for pid in $pids
+                kill $pid
+                echo Killed process with pid $pid and port $port
+            end
         end
     else
         echo "Usage: killport <port>"
