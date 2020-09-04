c1cbf0429662608e3b268a4379bdfe4f55a8a2bb
andys8 <andys8@users.noreply.github.com>
2020-09-04 03:23:52 +0200

    Script to install haskell language server
    Not using because too slow

---
+++ scripts/update-haskell-language-server.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+command -v "haskell-language-server" >/dev/null 2>&1 && {
+	VERSION=$(haskell-language-server --version)
+	echo "haskell-language-server is already installed."
+	echo "$VERSION"
+	exit 0
+}
+
+REPO=haskell/haskell-language-server
+REV=0.4.0
+FOLDER=haskell-language-server-$(shuf -i0-10000000 -n1)
+
+# install with stack
+echo ">> Installing haskell-language-server (building from source)"
+cd /tmp
+git clone -b $REV --recurse-submodules --single-branch https://github.com/$REPO "$FOLDER"
+cd "$FOLDER"
+stack install --stack-yaml stack-8.8.3.yaml
+echo ">> haskell-language-server installed with stack"
+exit 0
