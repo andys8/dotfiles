2b09b2a05b6505f929967e9663aee1d5184abdab
andys8 <andys8@users.noreply.github.com>
2020-10-13 00:04:33 +0200

    Bumping haskell-language server

--- scripts/update-haskell-language-server.sh
+++ scripts/update-haskell-language-server.sh
@@ -9,7 +9,7 @@ command -v "haskell-language-server" >/dev/null 2>&1 && {
 }
 
 REPO=haskell/haskell-language-server
-REV=0.5.0
+REV=0.5.1
 FOLDER=haskell-language-server-$(shuf -i0-10000000 -n1)
 
 # install with stack
