f85a2c5a39506a6142db0abae4f7bae73b439a03
andys8 <andys8@users.noreply.github.com>
2020-11-19 16:21:45 +0100

    todo script

--- scripts/link.sh
+++ scripts/link.sh
@@ -96,6 +96,7 @@ ln -sf ~/dotfiles/bin/lock ~/bin/lock
 ln -sf ~/dotfiles/bin/passmenu-otp ~/bin/passmenu-otp
 ln -sf ~/dotfiles/bin/script-rofi ~/bin/script-rofi
 ln -sf ~/dotfiles/bin/stack-test ~/bin/stack-test
+ln -sf ~/dotfiles/bin/todo ~/bin/todo
 ln -sf ~/dotfiles/bin/touchpad-toggle ~/bin/touchpad-toggle
 ln -sf ~/dotfiles/bin/webcam ~/bin/webcam
 
