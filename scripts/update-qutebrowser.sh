0eca076632d04701871bfdfa9e0d3c23e4d58fa2
andys8 <andys8@users.noreply.github.com>
2020-12-23 19:29:12 +0100

    Qutebrowser update script can update on debian/mint

--- scripts/update-qutebrowser.sh
+++ scripts/update-qutebrowser.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
+# Update qutebrowser on different operating systems
+
 set -euo pipefail
 
-command -v "qutebrowser" >/dev/null 2>&1 && {
-    VERSION=$(qutebrowser --version | grep "qutebrowser v")
-    echo "qutebrowser ($VERSION) is already installed."
-    exit 0
-}
+VERSION_MINT="1.14.1"
+VERSION_MINT_ALL="$VERSION_MINT-1"
 
 LINUX=$(lsb_release -i -s)
 LINUX=${LINUX,,}
 
+command -v "qutebrowser" >/dev/null 2>&1 && {
+    VERSION=$(qutebrowser --version | grep "qutebrowser v")
+    echo "qutebrowser ($VERSION) is already installed."
+
+    # Check if newer on mint
+    if [ "$LINUX" = "linuxmint" ] && [[ ! "$VERSION" == *"$VERSION_MINT"* ]]; then
+        echo "Will install version $VERSION_MINT_ALL"
+    else
+        exit 0
+    fi
+}
+
 if [ "$LINUX" = "linuxmint" ]; then
 
-    VERSION="1.13.1-1"
     BASE_URL=http://ftp.de.debian.org/debian/pool/main/q/qutebrowser
-    DEB1=qutebrowser_${VERSION}_all.deb
-    DEB2=qutebrowser-qtwebengine_${VERSION}_all.deb
+    DEB1=qutebrowser_${VERSION_MINT_ALL}_all.deb
+    DEB2=qutebrowser-qtwebengine_${VERSION_MINT_ALL}_all.deb
 
     echo "Download qutebrowser packages"
     curl "$BASE_URL/$DEB1" -o "/tmp/$DEB1"
