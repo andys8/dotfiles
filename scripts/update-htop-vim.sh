ae98d454e2d56e539f45aa087af3e8e9269cc418
andys8 <andys8@users.noreply.github.com>
2021-01-09 12:47:59 +0100

    htop-vim: Build from source
    * AUR package `htop-vim-git` often broken
    * Updates not really necessary that often

--- scripts/update-htop-vim.sh
+++ scripts/update-htop-vim.sh
@@ -7,40 +7,23 @@ command -v "htop" >/dev/null 2>&1 && {
     exit 0
 }
 
-LINUX=$(lsb_release -i -s)
-LINUX=${LINUX,,}
 
-if [ "$LINUX" = "linuxmint" ]; then
+REV=3.0.3vim
+TARBALL=https://github.com/KoffeinFlummi/htop-vim/tarball/$REV
+TMP_DIR=/tmp/htop-vim-installation-$REV
 
-    COMMIT=70c91d6d598f15b0311ae6779810e036133e72d2
-    TARBALL=https://github.com/KoffeinFlummi/htop-vim/tarball/$COMMIT
-    TMP_DIR=/tmp/htop-vim-installation-$COMMIT
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 
-    mkdir -p $TMP_DIR
-    cd $TMP_DIR
+# Download tarball
+curl -L $TARBALL | tar xz
 
-    # Download tarball
-    curl -L $TARBALL | tar xz
+# make binary
+cd $TMP_DIR/KoffeinFlummi-htop-vim-*
 
-    # make binary
-    cd $TMP_DIR/KoffeinFlummi-htop-vim-*
+./autogen.sh
+./configure
+make
 
-    ./autogen.sh
-    ./configure
-    make
-
-    echo "Please enter password for 'make install' (htop needs to be available for root user)"
-    sudo make install
-
-elif [ "$LINUX" = "manjarolinux" ]; then
-
-    ! command -v "yay" >/dev/null 2>&1 && {
-        echo "yay missing"
-        exit 1
-    }
-    yay -S htop-vim-git --noconfirm
-
-else
-    echo "Unexpected distribution: $LINUX"
-    exit 1
-fi
+echo "Please enter password for 'make install' (htop needs to be available for root user)"
+sudo make install
