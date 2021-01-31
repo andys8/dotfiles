30fddd7478f5690095c22dd35d8876fab191a9b9
andys8 <andys8@users.noreply.github.com>
2021-01-31 17:02:10 +0100

    Haskell-language-server: 0.9 + hiedb

--- scripts/update-haskell-language-server.sh
+++ scripts/update-haskell-language-server.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 REPO=haskell/haskell-language-server
-REV=0.8.0
+# Rev can be commit or version "x.x.x"
+REV=6b6c405d14a29ab3d2e7dbb4e2f79229758d26ba
 
 command -v "haskell-language-server" >/dev/null 2>&1 && {
-    VERSION=$(haskell-language-server --numeric-version)
+    VERSION=$(haskell-language-server --version)
     echo "haskell-language-server is installed"
-    echo "Expected: $REV (tag)"
-    echo "Actual: $VERSION (numeric version)"
+    echo "Expected: $REV"
+    echo "Actual: $VERSION"
     if [[ $VERSION =~ $REV ]]; then
         echo "-> Version is up-to-date"
         exit 0
@@ -21,8 +22,10 @@ command -v "haskell-language-server" >/dev/null 2>&1 && {
 FOLDER=haskell-language-server-$RANDOM
 echo ">> Installing haskell-language-server (building from source)"
 cd /tmp
-git clone -b $REV --recurse-submodules --single-branch https://github.com/$REPO "$FOLDER"
+git clone --recurse-submodules https://github.com/$REPO "$FOLDER"
 cd "$FOLDER"
+git checkout $REV
+git submodule update
 stack ./install.hs hls-8.8.3 hls-8.8.4
 echo ">> haskell-language-server installed with stack"
 exit 0
