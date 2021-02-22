9b37d1b520e708b46ca89cb95d98d1686be712e5
andys8 <andys8@users.noreply.github.com>
2021-02-22 23:40:09 +0100

    Qutebrowser: Firefox cmd

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -24,6 +24,7 @@ dracula.blood(c, { 'font': { 'size': 11 } })
 c.aliases = {
     'adblock-toggle': 'config-cycle -t content.blocking.enabled',
     'chromium': 'spawn --detach chromium {url}',
+    'firefox': 'spawn --detach firefox {url}',
     'incognito': 'open --private',
     'lastpass': 'hint --first inputs ;; spawn --userscript qute-lastpass',
     'mpv': 'spawn --detach mpv {url}',
