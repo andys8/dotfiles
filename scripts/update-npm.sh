0c49530f8c4573d5e776af0a95759012cd989cdb
andys8 <andys8@users.noreply.github.com>
2021-02-27 01:32:54 +0100

    Script to install purescript language server (fork)

--- scripts/update-npm.sh
+++ scripts/update-npm.sh
@@ -9,22 +9,21 @@ npm set update-notifier false
 
 # NPM installable packages
 packages=(
-    "andys8/purescript-language-server#andys8-fork" # purescript langauge server
-    asciicast2gif                                   # convert asciinema to gif
-    bash-language-server                            # bash ls
-    dockerfile-language-server-nodejs               # docker ls
-    elm-json                                        # update and change elm.json files
-    fx                                              # json viewer
-    http-server                                     # spin up http server in directory
-    prettier                                        # JS/TS/CSS/Markdown formatter
-    pscid                                           # purescript compiler daemon (like ghcid)
-    purescript                                      # purescript compiler (purs)
-    purty                                           # purescript formatter
-    qrcode-terminal                                 # Encode text as QR code
-    spago                                           # purescript bundler
-    typescript                                      # TS compiler
-    uglify-js                                       # minimize js
-    write-good                                      # check and improve written text
+    asciicast2gif                     # convert asciinema to gif
+    bash-language-server              # bash ls
+    dockerfile-language-server-nodejs # docker ls
+    elm-json                          # update and change elm.json files
+    fx                                # json viewer
+    http-server                       # spin up http server in directory
+    prettier                          # JS/TS/CSS/Markdown formatter
+    pscid                             # purescript compiler daemon (like ghcid)
+    purescript                        # purescript compiler (purs)
+    purty                             # purescript formatter
+    qrcode-terminal                   # Encode text as QR code
+    spago                             # purescript bundler
+    typescript                        # TS compiler
+    uglify-js                         # minimize js
+    write-good                        # check and improve written text
 )
 
 npm_config_loglevel=error npm install -g "${packages[@]}"
