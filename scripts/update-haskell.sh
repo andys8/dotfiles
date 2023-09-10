4031449259f7cd09f25f5825b216ab9859433c24
andys8 <andys8@users.noreply.github.com>
2023-09-10 15:27:03 +0200

    Update haskell compiler versions

--- scripts/update-haskell.sh
+++ scripts/update-haskell.sh
@@ -9,7 +9,7 @@ echo ">> Update ghcup"
 ghcup upgrade
 
 echo ">> Installing cabal"
-ghcup install cabal recommended --set
+ghcup install cabal latest --set
 
 echo ">> Cabal update"
 (cd /tmp && cabal update)
@@ -18,22 +18,16 @@ echo ">> Installing stack"
 ghcup install stack recommended --set
 
 echo ">> Installing ghc"
-ghcup install ghc 8.10.7 --no-set
+ghcup install ghc 9.6.2 --no-set
+ghcup install ghc 9.4.7 --no-set
 ghcup install ghc 9.2.8 --set
 
 echo ">> Checking haskell-language-server"
-hlsVersion="1.10.0.0"
+hlsVersion="2.2.0.0"
 
 if [[ $(haskell-language-server-wrapper --version) != *"$hlsVersion"* ]] || [[ "$arg" == "--force" ]]; then
     echo ">> Installing haskell-language-server ($hlsVersion)"
-    ghcup compile hls \
-        --git-ref "$hlsVersion" \
-        --git-describe-version \
-        --ghc 8.10.7 \
-        --ghc 9.2.8 \
-        --set \
-        -- --ghc-options='-dynamic' \
-        --flags="-haddockComments -eval -importLens -retrie -tactic -stan -alternateNumberFormat -gadt -explicitFixity -floskell"
+    ghcup install hls $hlsVersion --set
 fi
 
 echo
