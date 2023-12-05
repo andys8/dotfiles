4499884afaf2737a7972814590082c552eb0248f
andys8 <andys8@users.noreply.github.com>
2023-12-05 17:58:06 +0100

    Nix: Add go and git-brunch

--- nix/packages.nix
+++ nix/packages.nix
@@ -100,7 +100,8 @@ in
     gitAndTools.git-gone # prune merged branches
     gitAndTools.hub # github commandline tool
     glow # markdown viewer
-    # haskellPackages.git-brunch # git checkout branch tui
+    go # go language
+    haskellPackages.git-brunch # git checkout branch tui
     hlint # haskell linter
     hyperfine # benchmark (time replacement)
     lazygit # git terminal ui
