818fca3705b09d1381fab78cf7a093e50874e7a4
andys8 <andys8@users.noreply.github.com>
2024-08-02 01:53:18 +0200

    init macos

--- scripts/init-macos.sh
+++ scripts/init-macos.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 1000
 defaults write com.apple.dock no-bouncing -bool TRUE
 killall Dock
+
+# key repeat
+defaults write -g initialKeyRepeat -int 13
+defaults write -g KeyRepeat -int 1
