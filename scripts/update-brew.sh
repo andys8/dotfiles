0d616af7bc6177529783ee05e772e30d58f2b855
andys8 <andys8@users.noreply.github.com>
2024-08-02 03:08:34 +0200

    Brewfile

--- scripts/update-brew.sh
+++ scripts/update-brew.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Update the OS
+# Update brew
 set -eu
 
 echo "Update brew"
 
 brew update 
+
+# Brewfile (update via `brew bundle dump`)
+cd ~/dotfiles
+brew bundle --no-lock
+
 brew upgrade
