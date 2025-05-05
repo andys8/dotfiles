5ad5446c66006ecce1e6c78e6a1802597df8aecb
andys8 <andys8@users.noreply.github.com>
2025-05-05 15:19:40 +0200

    Cursor leader fix

--- nvim/cursor.lua
+++ nvim/cursor.lua
@@ -1,54 +1,64 @@
 -- Cursor-specific configuration
 -- This file contains settings specifically for Cursor IDE integration
 
+-- At the top of cursor.lua, before any keybinding
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
 -- Ensure cursor integration
 if vim.g.vscode then
-  -- VSCode/Cursor integration
-  print("Cursor IDE integration active")
-
+  -- Load the VSCode module
+  local vscode = require('vscode')
 
   -- Define Cursor-specific commands
   -- These commands are specific to Cursor functionality
 
+  -- Test basic VSCode action without leader key
+  vim.keymap.set('n', '<Space>e', function() vscode.action('workbench.action.quickOpen') end, { silent = true })
+
   -- Override key bindings with Cursor-specific commands
-  vim.keymap.set('n', '<leader>e', "<cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>f', "<cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>p', "<cmd>call VSCodeNotify('editor.action.formatDocument')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>d', "<cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>g', "<cmd>call VSCodeNotify('editor.action.showHover')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>r', "<cmd>call VSCodeNotify('editor.action.rename')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>s', "<cmd>call VSCodeNotify('editor.action.sourceAction')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>a', "<cmd>call VSCodeNotify('editor.action.marker.next')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>A', "<cmd>call VSCodeNotify('editor.action.marker.prev')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>i', "<cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>t', "<cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>u', "<cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", { silent = true })
+  vim.keymap.set('n', '<leader>e', function() vscode.action('workbench.action.quickOpen') end, { silent = true })
+  vim.keymap.set('n', '<leader>f', function() vscode.action('workbench.action.findInFiles') end, { silent = true })
+  vim.keymap.set('n', '<leader>p', function() vscode.action('editor.action.formatDocument') end, { silent = false })
+  vim.keymap.set('n', '<leader>d', function() vscode.action('editor.action.revealDefinition') end, { silent = true })
+  vim.keymap.set('n', '<leader>g', function() vscode.action('editor.action.showHover') end, { silent = true })
+  vim.keymap.set('n', '<leader>r', function() vscode.action('editor.action.rename') end, { silent = true })
+  vim.keymap.set('n', '<leader>s', function() vscode.action('editor.action.sourceAction') end, { silent = true })
+  vim.keymap.set('n', '<leader>a', function() vscode.action('editor.action.marker.next') end, { silent = true })
+  vim.keymap.set('n', '<leader>A', function() vscode.action('editor.action.marker.prev') end, { silent = true })
+  vim.keymap.set('n', '<leader>i', function() vscode.action('editor.action.goToImplementation') end, { silent = true })
+  vim.keymap.set('n', '<leader>t', function() vscode.action('editor.action.goToTypeDefinition') end, { silent = true })
+  vim.keymap.set('n', '<leader>u', function() vscode.action('editor.action.goToReferences') end, { silent = true })
 
   -- Multi cursor support
-  vim.keymap.set('n', '<C-n>', "<cmd>call VSCodeNotify('editor.action.addSelectionToNextFindMatch')<CR>", { silent = true })
-  vim.keymap.set('x', '<C-n>', "<cmd>call VSCodeNotify('editor.action.addSelectionToNextFindMatch')<CR>", { silent = true })
+  vim.keymap.set('n', '<C-n>', function() vscode.action('editor.action.addSelectionToNextFindMatch') end, { silent = true })
+  vim.keymap.set('x', '<C-n>', function() vscode.action('editor.action.addSelectionToNextFindMatch') end, { silent = true })
 
   -- Clear search highlighting
-  vim.keymap.set('n', '<leader><space>', "<cmd>call VSCodeNotify('editor.action.clearFind')<CR>", { silent = true })
+  vim.keymap.set('n', '<leader><space>', function() vscode.action('editor.action.clearFind') end, { silent = true })
 
   -- Save file
-  vim.keymap.set('n', '<C-s>', "<cmd>call VSCodeNotify('workbench.action.files.save')<CR>", { silent = true })
-  vim.keymap.set('i', '<C-s>', "<Esc><cmd>call VSCodeNotify('workbench.action.files.save')<CR>", { silent = true })
+  vim.keymap.set('n', '<C-s>', function() vscode.action('workbench.action.files.save') end, { silent = true })
+  vim.keymap.set('i', '<C-s>', function()
+    vscode.with_insert(function()
+      vscode.action('workbench.action.files.save')
+    end)
+  end, { silent = true })
 
   -- Buffer/tab navigation
-  vim.keymap.set('n', '<leader>x', "<cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>w', "<cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>z', "<cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>q', "<cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>", { silent = true })
-  vim.keymap.set('n', '<leader>c', "<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>", { silent = true })
+  vim.keymap.set('n', '<leader>x', function() vscode.action('workbench.action.nextEditor') end, { silent = true })
+  vim.keymap.set('n', '<leader>w', function() vscode.action('workbench.action.nextEditor') end, { silent = true })
+  vim.keymap.set('n', '<leader>z', function() vscode.action('workbench.action.previousEditor') end, { silent = true })
+  vim.keymap.set('n', '<leader>q', function() vscode.action('workbench.action.previousEditor') end, { silent = true })
+  vim.keymap.set('n', '<leader>c', function() vscode.action('workbench.action.closeActiveEditor') end, { silent = true })
 
   -- Window navigation
-  vim.keymap.set('n', '<C-h>', "<cmd>call VSCodeNotify('workbench.action.navigateLeft')<CR>", { silent = true })
-  vim.keymap.set('n', '<C-j>', "<cmd>call VSCodeNotify('workbench.action.navigateDown')<CR>", { silent = true })
-  vim.keymap.set('n', '<C-k>', "<cmd>call VSCodeNotify('workbench.action.navigateUp')<CR>", { silent = true })
-  vim.keymap.set('n', '<C-l>', "<cmd>call VSCodeNotify('workbench.action.navigateRight')<CR>", { silent = true })
+  vim.keymap.set('n', '<C-h>', function() vscode.action('workbench.action.navigateLeft') end, { silent = true })
+  vim.keymap.set('n', '<C-j>', function() vscode.action('workbench.action.navigateDown') end, { silent = true })
+  vim.keymap.set('n', '<C-k>', function() vscode.action('workbench.action.navigateUp') end, { silent = true })
+  vim.keymap.set('n', '<C-l>', function() vscode.action('workbench.action.navigateRight') end, { silent = true })
 
   -- Split windows
-  vim.keymap.set('n', '<Leader>h', "<cmd>call VSCodeNotify('workbench.action.splitEditorDown')<CR>", { silent = true })
-  vim.keymap.set('n', '<Leader>v', "<cmd>call VSCodeNotify('workbench.action.splitEditorRight')<CR>", { silent = true })
+  vim.keymap.set('n', '<Leader>h', function() vscode.action('workbench.action.splitEditorDown') end, { silent = true })
+  vim.keymap.set('n', '<Leader>v', function() vscode.action('workbench.action.splitEditorRight') end, { silent = true })
 end
